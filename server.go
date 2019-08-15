20610c7a19c80dd98d55b488d0bea98299f3dbb3
Ethan Allen <ethancallen@gmail.com>
2019-08-14 22:28:30 -0400

    added viper and argon2

--- server.go
+++ server.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+  // general
 	"errors"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
 	"os"
-	"log"
 
+  // web stuff
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/sessions"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/middleware"
 
+  // database stuff
 	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"strconv"
+
+  // config stuff
+	"github.com/spf13/viper"
+
+	"github.com/alexedwards/argon2id"
 )
 
 func accessible(c echo.Context) error {
@@ -75,7 +83,7 @@ func Login(c echo.Context) error {
 		log.Fatal(err)
 	}
 
-	rows, err := database.Query("SELECT id, username, password FROM users WHERE username=?",user)
+	rows, err := database.Query("SELECT id, username, password FROM users WHERE username=?", user)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -105,6 +113,74 @@ func Login(c echo.Context) error {
 }
 
 func main() {
+
+	// Load Configs
+	viper.AddConfigPath("./configs")
+	viper.AddConfigPath("$HOME/configs")
+	viper.SetConfigName("env")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+
+	// Confirm which config file is used
+	fmt.Printf("Using config: %s\n", viper.ConfigFileUsed())
+
+	port := viper.Get("prod.port") // returns string
+	//port := viper.GetInt("prod.port") // returns integer
+	fmt.Printf("Value: %v, Type: %T\n", port, port)
+
+
+/*
+	Changing the Parameters
+
+When creating a hash you can and should configure the parameters to be suitable for the environment that the code is running in. The parameters are:
+
+    Memory — The amount of memory used by the Argon2 algorithm (in kibibytes).
+    Iterations — The number of iterations (or passes) over the memory.
+    Parallelism — The number of threads (or lanes) used by the algorithm.
+    Salt length — Length of the random salt. 16 bytes is recommended for password hashing.
+    Key length — Length of the generated key (or password hash). 16 bytes or more is recommended.
+
+The Memory and Iterations parameters control the computational cost of hashing the password. The higher these figures are, the greater the cost of generating the hash and the longer the runtime. It also follows that the greater the cost will be for any attacker trying to guess the password.
+
+If the code is running on a machine with multiple cores, then you can decrease the runtime without reducing the cost by increasing the Parallelism parameter. This controls the number of threads that the work is spread across. Important note: Changing the value of the Parallelism parameter changes the hash output.
+
+params := &Params{
+	Memory:      128 * 1024,
+	Iterations:  4,
+	Parallelism: 4,
+	SaltLength:  16,
+	KeyLength:   32,
+}
+
+
+hash, err := argon2id.CreateHash("pa$$word", argon2id.DefaultParams)
+if err != nil {
+	log.Fatal(err)
+}
+
+For guidance and an outline process for choosing appropriate parameters see https://tools.ietf.org/html/draft-irtf-cfrg-argon2-04#section-4.
+*/
+	// CreateHash returns a Argon2id hash of a plain-text password using the
+	// provided algorithm parameters. The returned hash follows the format used
+	// by the Argon2 reference C implementation and looks like this:
+	// $argon2id$v=19$m=65536,t=3,p=2$c29tZXNhbHQ$RdescudvJCsgt3ub+b+dWRWJTmaaJObG
+	hash, err := argon2id.CreateHash("pa$$word", argon2id.DefaultParams)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// ComparePasswordAndHash performs a constant-time comparison between a
+	// plain-text password and Argon2id hash, using the parameters and salt
+	// contained in the hash. It returns true if they match, otherwise it returns
+	// false.
+	match, err := argon2id.ComparePasswordAndHash("pa$$word", hash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Match: %v", match)
+
 
 	// Database
 	database, _ := sql.Open("sqlite3", "./statmeet.db")
